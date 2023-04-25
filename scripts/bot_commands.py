dc9c0387ac7f5d12deeecbcbf2613762a5a9787a
Cory Coddington <123911856+Cytranics@users.noreply.github.com>
2023-04-24 23:45:23 -0400

    Update bot_commands.py
    Updated some quotes to fix gpt3.5
    
    Signed-off-by: Cory Coddington <123911856+Cytranics@users.noreply.github.com>

--- scripts/bot_commands.py
+++ scripts/bot_commands.py
@@ -10,24 +10,19 @@ botcommands = [
         "command_description": "Used when you do not need to use a command"
     },
     {
-        "command_string": "create_pdf_from_html_markup",
-        "command_argument": "Filename: [FILENAME] Content: [CONTENT]",
-        "command_description": "Create PDF's by sending formatted HTML as the content along with any research data."
-    },
-    {
         "command_string": "create_python_script",
         "command_argument": "Filename: [FILENAME] Content: [CONTENT]",
-        "command_description": "Writes and saves a formatted python script that do not require API keys. Replace '[FILENAME]' with the desired filename and '[CONTENT]' with the formatted python script (using triple quotes). Ensure you create a full working script with correct import modules, and verify non interactive scripts."
+        "command_description": "Writes and saves a formatted python script that does not require an API key to execute. Replace '[FILENAME]' with the desired filename and '[CONTENT]' with the formatted python script using triple quotes, do not send \line breaks. Ensure you create a full working script with correct import modules, and verify non interactive scripts."
     },
     {
         "command_string": "write_new_content_to_file",
         "command_argument": "Filename: [FILENAME] Content: [CONTENT]",
-        "command_description": "Write and save formatted text content to a file. Replace '[FILENAME]' with the desired filename and '[CONTENT]' with the content sent as a multiline string using triple quotes."
+        "command_description": "Write and save formatted text content to a single file. Replace '[FILENAME]' with the desired filename and '[CONTENT]' with the content sent as a multiline string using triple quotes."
     },
     {
         "command_string": "append_content_to_existing_file",
         "command_argument": "Filename: [FILENAME] Content: [CONTENT]",
-        "command_description": "Append formatted content or formatted code to existing files. Replace '[FILENAME]' with the desired filename and '[CONTENT]' with the file content as a multiline string (using triple quotes)."
+        "command_description": "Append formatted content or formatted code to an existing file. Replace '[FILENAME]' with the desired filename and '[CONTENT]' with the file content as a multiline string (using triple quotes)."
     },
     {
         "command_string": "read_content_from_file",
@@ -41,13 +36,13 @@ botcommands = [
     },
     {
         "command_string": "scrape_website_url",
-        "command_argument": "https://url",
-        "command_description": "Used to scrape the contents of a URL with http or https. Replace 'url' with the single web address you want to visit. Summerize the content when analyzing and visit every URL for thorough research"
+        "command_argument": "https://url|raw_html|max_length=#",
+        "command_description": "Used to scrape the contents of a URL with http or https. Replace 'url' with the single web address, add the raw_html option to gather the HTML markup and pass max_character to return the amount of characters you need. Example: https://example.com|raw_html|max_length=500"
     },
     {
         "command_string": "save_research",
         "command_argument": "Title: [CONTENT] ResearchContent: [CONTENT]",
-        "command_description": "Saves data gathered from the internet and through research. Format argument as Title: [CONTENT] ResearchContent: [CONTENT]"
+        "command_description": "Saves data gathered from the internet and through research. Format argument as Title: '[CONTENT]' ResearchContent: '[CONTENT]'"
     },
     {
         "command_string": "fetch_research",
@@ -62,7 +57,7 @@ botcommands = [
     {
         "command_string": "mission_accomplished",
         "command_argument": "Mission accomplishment message",
-        "command_description": "Marks the goal as completed if all tasks have been completed."
+        "command_description": "Use this command only if the goal has been 100% completed. If you were unable to complete steps, you must find another way as the goal will not be complete."
     },
 ]
 
